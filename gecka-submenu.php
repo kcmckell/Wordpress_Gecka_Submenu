e96aff69e666c804c770ce9d5500c6503ced1b65
Lox <lox.dev@knc.nc>
2010-06-22 16:20:20 +1100

    Added licence information

--- gecka-submenu.php
+++ gecka-submenu.php
@@ -2,14 +2,27 @@
 /*
 Plugin Name: Gecka Submenu
 Plugin URI: http://github.com/loxK/Wordpress_Gecka_Submenu
-Description: Provide submenu and autosubmenu widgets for the new wordpress 3.0 menu system
+Description: Provide submenu and autosubmenu widgets, submenu shortcode and submenu template tag for the new wordpress 3.0 menu system
 Version: 0.2
 Author: Laurent Dinclaux
 Author URI: http://gecka.nc
+Licence: GPL2
 */
 
-/* 
-Copyright 2010  Gecka SARL (email: contact@gecka.nc)
+/* Copyright 2010  Gecka SARL (email: contact@gecka.nc). All rights reserved
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 define('GKSM_PATH' , WP_PLUGIN_DIR . "/" . plugin_basename(dirname(__FILE__)) );
@@ -31,7 +44,3 @@ if (class_exists('Gecka_Submenu')) {
         include GKSM_PATH . '/template-tags.php';
     }
 }
-
-
-
-?>
